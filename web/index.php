d5252ddea2b1921f325bacb79403c258f56faf5b
James Mendoza <jMendoza@goldline.com>
2016-11-23 12:53:59 -0800

    added image for Cindy

--- web/index.php
+++ web/index.php
@@ -2,5 +2,7 @@
 <head></head>
 <body>
     Hello World! It's us, Lance Dolan &amp; James.
+    
+    <img src="https://www.goldline.com/sites/default/files/holiday-gifts-ltb.jpg" alt="Goldline LLC">
 </body>
 </html>
