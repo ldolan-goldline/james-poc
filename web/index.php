20e1d41278b1521b1595ea58cd7ddc780951efb0
James Mendoza <jMendoza@goldline.com>
2016-11-23 12:05:07 -0800

    added javascript date/time to homepage

--- web/index.php
+++ web/index.php
@@ -8,19 +8,20 @@
 <script type="application/javascript">
 var today = new Date();
 var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
+var mm = today.getMonth() + 1; //January is 0!
 var yyyy = today.getFullYear();
+var hr = today.getHours();
+var min = today.getMinutes();
+min = ('0' + min).slice(-2);
 
-if(dd<10) {
-    dd='0'+dd
-} 
+var ampm = hr >= 12 ? 'pm' : 'am';
+    
+if( dd<10 ) { dd ='0' + dd; }  
+if( mm<10 ) { mm ='0' + mm; }
 
-if(mm<10) {
-    mm='0'+mm
-} 
 
-today = mm+'/'+dd+'/'+yyyy;
-document.write(today + '.');
+today = mm +'/'+ dd +'/'+ yyyy;
+document.write(today +' '+ hr +':'+ min +' '+ ampm +'.');
 </script>
     
 </body>
