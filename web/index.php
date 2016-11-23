39b4e5d77d9f9d8fa6a6752d7c0304d2d40a927a
James Mendoza <jMendoza@goldline.com>
2016-11-23 11:43:20 -0800

    added more txt to homepage

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <html>
 <head></head>
 <body>
-    Hello World! It's us, Lance Dolan &amp; James.
+    Hello World! It's us, Lance Dolan &amp; James. The local time is 2016/11/23 11:42pm PST.
 </body>
 </html>
