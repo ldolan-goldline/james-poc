0fce67190c5082d9fbb492eec7bb37d20d9e98ad
James Mendoza <jMendoza@goldline.com>
2016-11-23 13:17:28 -0800

    moved datetime stuff into js file (MVC)

--- web/index.php
+++ web/index.php
@@ -7,26 +7,7 @@
 <body>
     Hello World! It's us, Lance Dolan &amp; James.<br><br>
 
-    The local time is 
-<script type="application/javascript">
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-var hr = today.getHours();
-var min = today.getMinutes();
-min = ('0' + min).slice(-2);
-
-var ampm = hr >= 12 ? 'pm' : 'am';
-    
-if( dd<10 ) { dd ='0' + dd; }  
-if( mm<10 ) { mm ='0' + mm; }
-
-
-today = mm +'/'+ dd +'/'+ yyyy;
-document.write(today +' '+ hr +':'+ min +' '+ ampm +'.');
-</script>
-
+    The local time is <span id="myDateTime">01/01/2017 12:00AM</span>.
     
     <div id="myNumber">
         this text will be changed with a number on page load.
--- web/js/pages/index.js
+++ web/js/pages/index.js
@@ -8,6 +8,26 @@ com.goldline.pagedriver = function() {
     var myNumberVm = new com.goldline.vm('myNumber');
     myNumberVm.updateInner(myNumber);
     
+    
+    
+    var currDate = new Date();
+    var currDay = currDate.getDate();
+    var currMonth = currDate.getMonth() + 1;
+    var currYear = currDate.getFullYear();
+    var currHour = currDate.getHours();
+    var currMinute = currDate.getMinutes();
+    var currAmPm = currHour >= 12 ? 'pm' : 'am';
+    
+    if( currDay < 10 ) { currDay = '0' + currDay; }  
+    if( currMonth < 10 ) { currMonth ='0' + currMonth; }    
+    if( currMinute < 10 ) { currMinute = '0' + currMinute}
+    if( currHour > 12) { currHour -= 12; }
+
+    var myDateTime = currMonth +'/'+ currDay +'/'+ currYear +' '+ currHour +":"+ currMinute +' '+ currAmPm;
+    var myDateTimeVm = new com.goldline.vm('myDateTime');
+    myDateTimeVm.updateInner(myDateTime);
+
+
 };
 
 document.addEventListener('DOMContentLoaded', function() {
